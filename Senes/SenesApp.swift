bfc2e237d0f7f5b7184ec2362c2adf8d4cc603d4
konradcr <63312007+konradcr@users.noreply.github.com>
2021-12-10 16:52:52 +0100

    Initial Commit

---
+++ Senes/SenesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SenesApp.swift
+//  Senes
+//
+//  Created by Konrad Cureau on 10/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct SenesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
